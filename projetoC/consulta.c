8da63fee806e5060c4b7f9bc7a343fa459a3c8fb
Gabriella Braga <79537610+gwedosun@users.noreply.github.com>
2025-05-07 09:08:01 -0300

    Update consulta.c

--- projetoC/consulta.c
+++ projetoC/consulta.c
@@ -60,6 +60,9 @@ void atualizarConsulta() {
 
     for (int i = 0; i < contador; i++) {
         if (consultas[i].id == id) {
+            printf("Novo nome do paciente: (Caso nao deseje alterar, repita o nome.)");
+            scanf(" %[^\n]", consultas[i].paciente);
+            printf("Nova data de nascimento: (Caso nao deseje alterar, repita a data.)");
             printf("Novo nome do paciente: (Caso nao deseje alterar, repita o nome.");
             scanf(" %[^\n]", consultas[i].paciente);
             printf("Nova data de nascimento: (Caso nao deseje alterar, repita a data.");
